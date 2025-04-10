b5eca686007da5d255161037ad3390577e060dba
REI209 <138412628+REI209@users.noreply.github.com>
2025-04-10 11:33:27 +0900

    end

--- Scene/EndScene.cpp
+++ Scene/EndScene.cpp
@@ -1,1 +1,31 @@
 #include "EndScene.h"
+
+EndScene::EndScene()
+{
+
+}
+
+EndScene::~EndScene()
+{
+
+}
+
+void EndScene::Initialize()
+{
+
+}
+
+void EndScene::Update()
+{
+
+}
+
+void EndScene::Draw()
+{
+	
+}
+
+void EndScene::Finalize()
+{
+
+}
--- Scene/EndScene.h
+++ Scene/EndScene.h
@@ -1,5 +1,14 @@
 #pragma once
 class EndScene
 {
+
+public:
+	EndScene();
+	~EndScene();
+
+    void Initialize();
+	void Update();
+	void Draw();
+	void Finalize();
 };
 
