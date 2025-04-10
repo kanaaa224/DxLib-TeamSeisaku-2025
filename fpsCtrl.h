76ba2203781f38b30ef68651848805a98ec52a2c
Kanato Shimabukuro <kanato.shimabukuro@gmail.com>
2025-04-10 11:03:57 +0900

    [ new ] FPSクラス
    FPSの制限、取得が可能

---
+++ fpsCtrl.h
@@ -0,0 +1,32 @@
+﻿//////////////////////////////////////////////////
+// FPS コントローラー
+// 作成: Kanato Shimabukuro
+//////////////////////////////////////////////////
+
+#pragma once
+
+#include "DxLib.h"
+
+// FPS コントローラー クラス（FPSの制限・取得に対応）
+class FPSCtrl {
+private:
+    static int frameTime, waitTime, lastTime, nowTime, updateTime, lastUpdate;
+
+    static float count, fps;
+
+public:
+    // 制限するFPSの値を設定
+    static void SetLimitRate(float);
+
+    // 現在のFPSを計算する間隔を設定
+    static void SetUpdateInterval(int);
+
+    // 事前に指定したFPSに制限
+    static void Limit();
+
+    // 事前に指定した間隔で現在のFPSを計算
+    static void Update();
+
+    // FPSを取得（注意：float型）
+    static float Get();
+};
